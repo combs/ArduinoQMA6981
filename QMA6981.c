6f17ffdeb8c1a23d25525107248855597846cc5d
Chris Combs <ccombs@gmail.com>
2020-11-13 17:36:34 -0500

    rework as class

--- QMA6981.c
+++ QMA6981.c
@@ -19,161 +19,6 @@
 #include <Wire.h>
 
 
-#define QMA6981_ADDR 0x12
-
-/*============================================================================
- * Register addresses and definitions
- *==========================================================================*/
-
-/* For the bandwidth register */
-
-typedef enum
-{
-    QMA6981_BW_3_9  = 0b000,
-    QMA6981_BW_7_8  = 0b001,
-    QMA6981_BW_15_6 = 0b010,
-    QMA6981_BW_31_2 = 0b011,
-    QMA6981_BW_62_5 = 0b100,
-    QMA6981_BW_125  = 0b101,
-    QMA6981_BW_250  = 0b110,
-    QMA6981_BW_500  = 0b111,
-} QMA6981_BANDWIDTH;
-
-typedef union
-{
-    uint8_t val;
-    struct
-    {
-        QMA6981_BANDWIDTH BW : 5;
-        bool ODRH            : 1;
-    } bitmask;
-} QMA6981_BW_VAL;
-
-/* For the range register */
-
-enum
-{
-    QMA6981_RANGE_2G  = 0x01,
-    QMA6981_RANGE_4G  = 0x02,
-    QMA6981_RANGE_8G  = 0x04,
-};
-
-/* For interrupt configuration register */
-
-typedef union
-{
-    uint8_t val;
-    struct
-    {
-        bool res               : 1;
-        bool STEP_UNSIMILAR_EN : 1;
-        bool STEP_QUIT_EN      : 1;
-        bool STEP_EN           : 1;
-        bool D_TAP_EN          : 1;
-        bool S_TAP_EN          : 1;
-        bool ORIENT_EN         : 1;
-        bool FOB_EN            : 1;
-    } bitmask;
-} QMA6981_INT_EN0_VAL;
-
-typedef union
-{
-    uint8_t val;
-    struct
-    {
-        bool HIGH_EN_X         : 1;
-        bool HIGH_EN_Y         : 1;
-        bool HIGH_EN_Z         : 1;
-        bool LOW_EN            : 1;
-        bool DATA_EN           : 1;
-        bool FFULL_EN          : 1;
-        bool FWM_EN            : 1;
-        bool res               : 1;
-    } bitmask;
-} QMA6981_INT_EN1_VAL;
-
-
-/* For the power level value register */
-
-typedef enum
-{
-    Tpreset_12us   = 0b00,
-    Tpreset_96us   = 0b01,
-    Tpreset_768us  = 0b10,
-    Tpreset_2048us = 0b11
-} QMA6981_POWER_VAL_PRESET;
-
-typedef enum
-{
-    SLEEP_DUR_FULL_SPEED = 0b0000,
-    SLEEP_DUR_0_5ms      = 0b0001,
-    SLEEP_DUR_1ms        = 0b0110,
-    SLEEP_DUR_2ms        = 0b0111,
-    SLEEP_DUR_4ms        = 0b1000,
-    SLEEP_DUR_6ms        = 0b1001,
-    SLEEP_DUR_10ms       = 0b1010,
-    SLEEP_DUR_25ms       = 0b1011,
-    SLEEP_DUR_50ms       = 0b1100,
-    SLEEP_DUR_100ms      = 0b1101,
-    SLEEP_DUR_500ms      = 0b1110,
-    SLEEP_DUR_1000ms     = 0b1111,
-} QMA6981_POWER_VAL_SLEEP_DUR;
-
-typedef union
-{
-    uint8_t val;
-    struct
-    {
-        QMA6981_POWER_VAL_SLEEP_DUR SLEEP_DUR : 4;
-        QMA6981_POWER_VAL_PRESET PRESET       : 2;
-        bool res                              : 1;
-        bool MODE_BIT                         : 1;
-    } bitmask;
-} QMA6981_POWER_VAL;
-
-/* Soft reset register value */
-
-#define QMA6981_SOFT_RESET_ALL_REGISTERS 0xB6
-
-/* Register addresses */
-
-typedef enum
-{
-    QMA6981_CHIP_ID      = 0x00,
-    QMA6981_DATA         = 0x01,
-    QMA6981_STEP_CNT     = 0x07,
-    QMA6981_INT_STATUS   = 0x0A,
-    QMA6981_FIFO_STATUS  = 0x0E,
-    QMA6981_FULL_SCALE   = 0x0F,
-    QMA6981_BW           = 0x10,
-    QMA6981_POWER_MODE   = 0x11,
-    QMA6981_STEP_CONF    = 0x13,
-    QMA6981_INT_EN       = 0x16,
-    QMA6981_INT_SRC      = 0x18,
-    QMA6981_INT_MAP      = 0x19,
-    QMA6981_INT_PIN_CONF = 0x20,
-    QMA6981_INT_LATCH    = 0x21,
-    QMA6981_LowG_HighG   = 0x22,
-    QMA6981_OS_CUST      = 0x27,
-    QMA6981_TAP_1        = 0x2A,
-    QMA6981_TAP_2        = 0x2B,
-    QMA6981__4D_6D       = 0x2C,
-    QMA6981_FIFO_WM      = 0x31,
-    QMA6981_SelfTest     = 0x32,
-    QMA6981_NVM_CFG      = 0x33,
-    QMA6981_SOFT_RESET   = 0x36,
-    QMA6981_IMAGE        = 0x37,
-    QMA6981_FIFO_CONF    = 0x3E,
-} QMA6981_reg_addr;
-
-/*============================================================================
- * Function prototypes
- *==========================================================================*/
-
-uint8_t QMA6981_writereg(QMA6981_reg_addr addr, uint8_t data);
-uint8_t QMA6981_readreg(QMA6981_reg_addr addr, uint8_t len, uint8_t* data);
-int16_t convertTwosComplement10bit(uint16_t in);
-
 /*============================================================================
  * Variables
  *==========================================================================*/
@@ -183,7 +28,17 @@ accel_t lastKnownAccel = {0};
 /*============================================================================
  * Functions
  *==========================================================================*/
+/**
+ * @brief Constructor
+ * 
+ * 
+*/
 
+void QMA6981() 
+{
+    interrupt1.value = 0;
+    interrupt2.value = 0;
+}
 /**
  * @brief Write a single byte of data to the given QMA6981 register
  *
@@ -191,7 +46,7 @@ accel_t lastKnownAccel = {0};
  * @param data The single byte to write
  * @return true if the data was read, false if there was an i2c error
  */
-uint8_t QMA6981_writereg(QMA6981_reg_addr addr, uint8_t data)
+uint8_t QMA6981::writeRegister(QMA6981_reg_addr addr, uint8_t data)
 {
     Wire.beginTransmission(QMA6981_ADDR);
     uint8_t writeCmd[2] = {addr, data};
@@ -207,7 +62,7 @@ uint8_t QMA6981_writereg(QMA6981_reg_addr addr, uint8_t data)
  * @param data A pointer to read the bytes into
  * @return true if the data was read, false if there was an i2c error
  */
-uint8_t QMA6981_readreg(QMA6981_reg_addr addr, uint8_t len, uint8_t* data)
+uint8_t QMA6981::readRegister(QMA6981_reg_addr addr, uint8_t len, uint8_t* data)
 {
     Wire.beginTransmission(QMA6981_ADDR);
     uint8_t reg[1] = {addr};
@@ -229,8 +84,20 @@ uint8_t QMA6981_readreg(QMA6981_reg_addr addr, uint8_t len, uint8_t* data)
  *
  * @return true if initialization succeeded, false if it failed
  */
-bool QMA6981_setup(void)
+bool QMA6981::begin(uint8_t address)
 {
+    _address = address;
+
+    uint8_t chip_id;
+    if(0 != readRegister(QMA6981_CHIP_ID, 1, chip_id)) {
+        // read failure;
+        return false;
+    }
+    if ((chip_id < 0xB0) || (chip_id > 0xBF)) {
+        // did not return proper chip ID
+        return false;
+    }
+
     QMA6981_POWER_VAL active =
     {
         .bitmask.MODE_BIT = true,
@@ -238,16 +105,16 @@ bool QMA6981_setup(void)
         .bitmask.SLEEP_DUR = SLEEP_DUR_FULL_SPEED,
         .bitmask.PRESET = Tpreset_12us
     };
-    if(0 != QMA6981_writereg(QMA6981_POWER_MODE, active.val))
+    if(0 != writeRegister(QMA6981_POWER_MODE, active.val))
     {
         return false;
     }
 
-    if(0 != QMA6981_writereg(QMA6981_SOFT_RESET, QMA6981_SOFT_RESET_ALL_REGISTERS))
+    if(0 != writeRegister(QMA6981_SOFT_RESET, QMA6981_SOFT_RESET_ALL_REGISTERS))
     {
         return false;
     }
-    if(0 != QMA6981_writereg(QMA6981_POWER_MODE, active.val))
+    if(0 != writeRegister(QMA6981_POWER_MODE, active.val))
     {
         return false;
     }
@@ -258,17 +125,17 @@ bool QMA6981_setup(void)
         .bitmask.ODRH = false,
         .bitmask.BW = QMA6981_BW_31_2
     };
-    if(0 != QMA6981_writereg(QMA6981_BW, bandwidth.val))
+    if(0 != writeRegister(QMA6981_BW, bandwidth.val))
     {
         return false;
     }
 
-    if(0 != QMA6981_writereg(QMA6981_FULL_SCALE, QMA6981_RANGE_2G))
+    if(0 != writeRegister(QMA6981_FULL_SCALE, QMA6981_RANGE_2G))
     {
         return false;
     }
 
-    if(0 != QMA6981_writereg(QMA6981_POWER_MODE, active.val))
+    if(0 != writeRegister(QMA6981_POWER_MODE, active.val))
     {
         return false;
     }
@@ -281,11 +148,11 @@ bool QMA6981_setup(void)
  *
  * @param currentAccel A pointer where the acceleration data will be stored
  */
-void QMA6981_poll(accel_t* currentAccel)
+void QMA6981::poll(accel_t* currentAccel)
 {
     // Read 7 bytes of data(0x00)
     uint8_t raw_data[6];
-    if(0 != QMA6981_readreg(QMA6981_DATA, 6, raw_data))
+    if(0 != readRegister(QMA6981_DATA, 6, raw_data))
     {
         // os_printf("read xyz error!!!\n");
         // Try reinitializing, then return last known value
@@ -303,44 +170,63 @@ void QMA6981_poll(accel_t* currentAccel)
     currentAccel->x = lastKnownAccel.x;
     currentAccel->y = lastKnownAccel.y;
     currentAccel->z = lastKnownAccel.z;
+    return currentAccel;
 }
 
-bool QMA6981_set_interrupts(uint8_t interrupt_number, bool step_unsimilar, bool step_quit, bool step, bool d_tap, bool s_tap, bool orient, bool fob, bool fwm, bool ffull, bool data, bool low, bool high_en_z, bool high_en_y, bool high_en_x)
+void QMA6981::poll()
 {
+    accel_t currentAccel;
+    return poll(currentAccel);
+}
 
-    QMA6981_INT_EN0_VAL byte0 =
+bool QMA6981::enableIinterrupts(uint8_t interrupt_pin, QMA6981_INT_EN_VAL interrupt_value)
+{
+    if (interrupt_pin == 1) {
+        interrupt1.value = interrupt_value.value;
+
+        if(0 != writeRegister(QMA6981_INT_MAP + 1, interrupt_value.value >> 8))
+        {
+            return false;
+        }
+        if(0 != writeRegister(QMA6981_INT_MAP, combined & 255))
+        {
+            return false;
+        }
+    } else if (interrupt_pin == 2)
     {
-        .bitmask.STEP_UNSIMILAR_EN = step_unsimilar,
-        .bitmask.STEP_QUIT_EN = step_quit,
-        .bitmask.STEP_EN = step,
-        .bitmask.D_TAP_EN = d_tap,
-        .bitmask.S_TAP_EN = s_tap,
-        .bitmask.ORIENT_EN = orient,
-        .bitmask.FOB_EN = fob
-    };
-    if(0 != QMA6981_writereg(QMA6981_INT_EN, byte0.val))
+        interrupt2.value = interrupt_value.value;
+
+        if(0 != writeRegister(QMA6981_INT_MAP + 3, interrupt_value.value >> 8))
+        {
+            return false;
+        }
+        if(0 != writeRegister(QMA6981_INT_MAP + 2, combined & 255))
+        {
+            return false;
+        }
+    } 
+    
+
+    uint16_t combined = interrupt1.value | interrupt2.value; 
+
+    if(0 != writeRegister(QMA6981_INT_EN + 1, combined >> 8))
     {
         return false;
     }
-    QMA6981_INT_EN1_VAL byte1 =
+    if(0 != writeRegister(QMA6981_INT_EN, combined & 255))
     {
-        .bitmask.HIGH_EN_X = high_en_x,
-        .bitmask.HIGH_EN_Y = high_en_y,
-        .bitmask.HIGH_EN_Z = high_en_z,
-        .bitmask.LOW_EN = low,
-        .bitmask.DATA_EN = data,
-        .bitmask.FFULL_EN = ffull,
-        .bitmask.FWM_EN = fwm
-    };
-    if(0 != QMA6981_writereg(QMA6981_INT_EN + 1, byte1.val))
+        return false;
+    } 
+    return true;
+}
+
+bool QMA6981::setInterruptType(QMA6981_INT_PIN_CONF_VAL configuration)
+{
+    if (0 != writeRegister(QMA6981_INT_PIN_CONF, configuration.value))
     {
         return false;
     }
-
- // not yet mapped to either interrupt pin.
- // to do so with smarts will require storing state.
- // needs to be a class.
- 
+    return true;
 }
 
 
@@ -351,7 +237,7 @@ bool QMA6981_set_interrupts(uint8_t interrupt_number, bool step_unsimilar, bool 
  * @param in
  * @return int16_t convertTwosComplement10bit
  */
-int16_t convertTwosComplement10bit(uint16_t in)
+int16_t QMA6981::convertTwosComplement10bit(uint16_t in)
 {
     if(in & 0x200)
     {
@@ -362,3 +248,5 @@ int16_t convertTwosComplement10bit(uint16_t in)
         return (in & 0x01FF); // make sure the sign bits are cleared
     }
 }
+
+
--- QMA6981.h
+++ QMA6981.h
@@ -8,7 +8,20 @@
 #ifndef QMA6981_H_
 #define QMA6981_H_
 
+#if ARDUINO >= 100
 #include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <Wire.h>
+
+
+
+/* Soft reset register value */
+
+#define QMA6981_SOFT_RESET_ALL_REGISTERS 0xB6
+
+#define QMA6981_ADDR 0x12
 
 
 typedef struct
@@ -18,9 +31,172 @@ typedef struct
     int16_t z;
 } accel_t;
 
-bool QMA6981_setup(void);
-void QMA6981_poll(accel_t* currentAccel);
-bool QMA6981_enable_interrupts(uint8_t interrupt_number, bool step_unsimilar, bool step_quit, bool step, bool d_tap, bool s_tap, bool orient, bool fob, bool fwm, bool ffull, bool data, bool low, bool high_en_z, bool high_en_y, bool high_en_x);
 
+
+/*============================================================================
+ * Register addresses and definitions
+ *==========================================================================*/
+
+/* For the bandwidth register */
+
+typedef enum
+{
+    QMA6981_BW_3_9  = 0b000,
+    QMA6981_BW_7_8  = 0b001,
+    QMA6981_BW_15_6 = 0b010,
+    QMA6981_BW_31_2 = 0b011,
+    QMA6981_BW_62_5 = 0b100,
+    QMA6981_BW_125  = 0b101,
+    QMA6981_BW_250  = 0b110,
+    QMA6981_BW_500  = 0b111,
+} QMA6981_BANDWIDTH;
+
+typedef union
+{
+    uint8_t val;
+    struct
+    {
+        QMA6981_BANDWIDTH BW : 5;
+        bool ODRH            : 1;
+    } bitmask;
+} QMA6981_BW_VAL;
+
+/* For the range register */
+
+enum
+{
+    QMA6981_RANGE_2G  = 0x01,
+    QMA6981_RANGE_4G  = 0x02,
+    QMA6981_RANGE_8G  = 0x04,
+};
+
+/* For interrupt configuration register */
+
+typedef union
+{
+    uint16_t val;
+    struct
+    {
+        bool HIGH_EN_X         : 1;
+        bool HIGH_EN_Y         : 1;
+        bool HIGH_EN_Z         : 1;
+        bool LOW_EN            : 1;
+        bool DATA_EN           : 1;
+        bool FFULL_EN          : 1;
+        bool FWM_EN            : 1;
+        bool res               : 2;
+        bool STEP_UNSIMILAR_EN : 1;
+        bool STEP_QUIT_EN      : 1;
+        bool STEP_EN           : 1;
+        bool D_TAP_EN          : 1;
+        bool S_TAP_EN          : 1;
+        bool ORIENT_EN         : 1;
+        bool FOB_EN            : 1;
+    } bitmask;
+} QMA6981_INT_EN_VAL;
+
+typedef union
+{
+    uint16_t val;
+    struct
+    {
+        bool INT1_LVL          : 1;
+        bool INT1_OD           : 1;
+        bool INT2_LVL          : 1;
+        bool INT2_OD           : 1;
+        bool res               : 4;
+    } bitmask;
+} QMA6981_INT_PIN_CONF_VAL;
+
+/* For the power level value register */
+
+typedef enum
+{
+    Tpreset_12us   = 0b00,
+    Tpreset_96us   = 0b01,
+    Tpreset_768us  = 0b10,
+    Tpreset_2048us = 0b11
+} QMA6981_POWER_VAL_PRESET;
+
+typedef enum
+{
+    SLEEP_DUR_FULL_SPEED = 0b0000,
+    SLEEP_DUR_0_5ms      = 0b0001,
+    SLEEP_DUR_1ms        = 0b0110,
+    SLEEP_DUR_2ms        = 0b0111,
+    SLEEP_DUR_4ms        = 0b1000,
+    SLEEP_DUR_6ms        = 0b1001,
+    SLEEP_DUR_10ms       = 0b1010,
+    SLEEP_DUR_25ms       = 0b1011,
+    SLEEP_DUR_50ms       = 0b1100,
+    SLEEP_DUR_100ms      = 0b1101,
+    SLEEP_DUR_500ms      = 0b1110,
+    SLEEP_DUR_1000ms     = 0b1111,
+} QMA6981_POWER_VAL_SLEEP_DUR;
+
+typedef union
+{
+    uint8_t val;
+    struct
+    {
+        QMA6981_POWER_VAL_SLEEP_DUR SLEEP_DUR : 4;
+        QMA6981_POWER_VAL_PRESET PRESET       : 2;
+        bool res                              : 1;
+        bool MODE_BIT                         : 1;
+    } bitmask;
+} QMA6981_POWER_VAL;
+
+/* Register addresses */
+
+typedef enum
+{
+    QMA6981_CHIP_ID      = 0x00,
+    QMA6981_DATA         = 0x01,
+    QMA6981_STEP_CNT     = 0x07,
+    QMA6981_INT_STATUS   = 0x0A,
+    QMA6981_FIFO_STATUS  = 0x0E,
+    QMA6981_FULL_SCALE   = 0x0F,
+    QMA6981_BW           = 0x10,
+    QMA6981_POWER_MODE   = 0x11,
+    QMA6981_STEP_CONF    = 0x13,
+    QMA6981_INT_EN       = 0x16,
+    QMA6981_INT_SRC      = 0x18,
+    QMA6981_INT_MAP      = 0x19,
+    QMA6981_INT_PIN_CONF = 0x20,
+    QMA6981_INT_LATCH    = 0x21,
+    QMA6981_LowG_HighG   = 0x22,
+    QMA6981_OS_CUST      = 0x27,
+    QMA6981_TAP_1        = 0x2A,
+    QMA6981_TAP_2        = 0x2B,
+    QMA6981__4D_6D       = 0x2C,
+    QMA6981_FIFO_WM      = 0x31,
+    QMA6981_SelfTest     = 0x32,
+    QMA6981_NVM_CFG      = 0x33,
+    QMA6981_SOFT_RESET   = 0x36,
+    QMA6981_IMAGE        = 0x37,
+    QMA6981_FIFO_CONF    = 0x3E,
+} QMA6981_reg_addr;
+
+/*============================================================================
+ * Function prototypes
+ *==========================================================================*/
+
+class QMA6981 : {
+    public:
+        QMA6981();
+        bool setup(uint8_t address = QMA6981_ADDR);
+        uint8_t writeRegister(QMA6981_reg_addr addr, uint8_t data);
+        uint8_t readRegister(QMA6981_reg_addr addr, uint8_t len, uint8_t* data);
+        int16_t convertTwosComplement10bit(uint16_t in);
+        accel_t poll(accel_t* currentAccel);
+        accel_t poll();
+        enableInterrupts(uint8_t interrupt_pin, QMA6981_INT_EN_VAL interrupt_value);
+        setInterruptType(uint8_t interrupt_pin, bool openDrain, bool logicTrueValue);
+    private:
+        uint8_t _address = QMA6981_ADDR;
+        QMA6981_INT_EN_VAL interrupt1;
+        QMA6981_INT_EN_VAL interrupt2;        
+
+}
 
 #endif /* QMA6981_H_ */
